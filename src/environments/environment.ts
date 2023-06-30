b75f2fe680e74609fb05165ef55cfa1fdf18557f
ElenaBez <131905015+ElenaBez@users.noreply.github.com>
2023-06-30 20:32:44 +0100

    newscommit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,29 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false
+const environment = {
+  production: false,
+  API_URL: 'https://newsapi.org/v2',
+  API_KEY: 'fe6aedf9ebb5f9db48998c6e307ed05b'
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+import { environment } from 'src/environments/environment';
+import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
+
+const API_URL = 'https://newsapi.org/v2';
+const API_KEY = 'b0d6e4609dff4b86b45e86e8b38b712d';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  constructor(public httpClient:HttpClient) {
+    this.loadData()
+  }
+
+  loadData(){
+      this.httpClient.get(`${API_URL}/top-headlines?country=${"Ireland"}&appid=${API_KEY}`).subscribe(results =>{
+        console.log(results);
+    })
+  }
+}

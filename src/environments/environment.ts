59558b55edb91bc20635f0567d05c5f827dc3ebc
ElenaBez <131905015+ElenaBez@users.noreply.github.com>
2023-06-30 20:58:46 +0100

    assignment 2 commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
+
 const environment = {
   production: false,
   API_URL: 'https://newsapi.org/v2',
   API_KEY: 'fe6aedf9ebb5f9db48998c6e307ed05b'
 };
 
-import { environment } from 'src/environments/environment';
 import { Component } from '@angular/core';
-import { HttpClient } from "@angular/common/http"
+import { HttpClient } from '@angular/common/http';
 
 const API_URL = 'https://newsapi.org/v2';
 const API_KEY = 'b0d6e4609dff4b86b45e86e8b38b712d';
@@ -17,13 +17,15 @@ const API_KEY = 'b0d6e4609dff4b86b45e86e8b38b712d';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(public httpClient:HttpClient) {
-    this.loadData()
+  constructor(public httpClient: HttpClient) {
+    this.loadData();
   }
 
-  loadData(){
-      this.httpClient.get(`${API_URL}/top-headlines?country=${"Ireland"}&appid=${API_KEY}`).subscribe(results =>{
+  loadData() {
+    this.httpClient
+      .get(`${API_URL}/top-headlines?country=Ireland&apiKey=${API_KEY}`)
+      .subscribe((results) => {
         console.log(results);
-    })
+      });
   }
 }
